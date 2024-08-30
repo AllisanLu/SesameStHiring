967124e212b46b1f8b76d54526671d3699a797f3
matts04 <matthew.bak@gmail.com>
2024-08-30 16:18:25 -0400

    changed the cors port

--- backend/src/main/java/org/example/backend/config/WebConfig.java
+++ backend/src/main/java/org/example/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:3000") // Add your frontend origin here
+                .allowedOrigins("http://localhost:5173") // Add your frontend origin here
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies
